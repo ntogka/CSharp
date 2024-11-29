87649cc87cf67a10f88fc221a2b63ce54d08e820
Marina Ntogka <marinadoga98@gmail.com>
2024-11-29 13:19:19 +0200

    Add files via upload

---
+++ ExtensionMethodsSourceCode/ExtensionDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using CustomExtensions;
+using System;
+
+namespace ExtensionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            "This is a demo".PrintToConsole();
+
+            ISimpleLogger logger = new SimpleLogger();
+            logger.Log("Test Error", "ERror");
+            logger.LogError("This is an error");
+            logger.LogWarning("This is a warning.");
+            Console.WriteLine("Goodbye");
+
+            // person.Print().LogInfo().SaveToDatabase(myDBConnection);
+            // SaveToDatabase(LogInfo(Print(person)));
+        }
+    }
+}
---
+++ ExtensionMethodsSourceCode/ExtensionDemo/SimpleLogger.cs
@@ -0,0 +1,15 @@
+﻿namespace ExtensionDemo
+{
+    public class SimpleLogger : ISimpleLogger
+    {
+        public void Log(string message)
+        {
+            System.Console.WriteLine(message);
+        }
+
+        public void Log(string message, string messageType)
+        {
+            Log($"{messageType}: {message}");
+        }
+    }
+}
